e675d3c6d03edb9e951bbfbdbdfcc2cf216a0c03
Anton Tsvetkov <Anton_Tsvetkov@epam.com>
2021-02-04 17:19:06 +0300

    add test for conversion according to priority

--- src/test/java/com/epam/laboratory/AppTest.java
+++ src/test/java/com/epam/laboratory/AppTest.java
@@ -5,5 +5,12 @@ import org.junit.Test;
 
 public class AppTest {
 
+    private static final String initialString = "2 + 3 * 45.3 * 90 + 20 - 8 / 20 - sqrt4";
+    private static final String prioritizedString = "3*45.3*90-8/20-sqrt4+2+20";
+
+    @Test
+    public void testConversionAccordingToPriority() {
+        Assert.assertEquals(conversionAccordingToPriority(initialString), prioritizedString);
+    }
 
 }
