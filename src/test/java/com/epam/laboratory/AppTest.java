bce2bd3ccd8037af2784dc926e5775f69bd84564
Anton Tsvetkov <anton_tsvetkov@epam.com>
2021-02-09 10:06:11 +0300

    Add new tests for subdivision math expression

--- src/test/java/com/epam/laboratory/AppTest.java
+++ src/test/java/com/epam/laboratory/AppTest.java
@@ -2,17 +2,40 @@ package com.epam.laboratory;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class AppTest {
 
     private static final String initialString = "2 + 3 * 45.3 * 90 + 20 - 8/20 - sqrt4";
-    private static final String prioritizedString = "3 * 45.3 * 90 - 8/20 - sqrt4 + 2 + 20";
+    private static final List<Double> numbers = Arrays.asList(
+            2.0, 3.0, 45.3, 90.0, 20.0, 8.0, 20.0, 4.0
+    );
+    private static final List<Character> arithmeticOperations = Arrays.asList(
+            '+', '*', '*', '+', '-', '/', 's'
+    );
     private static final double answer = 12_250.6;
 
-    Calculator calculator = new Calculator();
+    private static final Calculator calculator = new Calculator();
 
+    @BeforeAll
     @Test
-    public void testCalculateMathExpression() {
+    public static void testExtractionNumbersFromMathExpression() {
+        Assert.assertEquals(calculator.extractionNumbersFromMathExpression(initialString), numbers);
+    }
+
+    @BeforeAll
+    @Test
+    public static void testExtractionArithmeticOperationsFromMathExpression() {
+        Assert.assertEquals(calculator.extractionArithmeticOperationsFromMathExpression(initialString), arithmeticOperations);
+    }
+
+    @AfterAll
+    @Test
+    public static void testCalculateMathExpression() {
         Assert.assertEquals(calculator.calculateMathExpression(initialString), answer, 0.0);
     }
 
