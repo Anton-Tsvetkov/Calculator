bd3a993ba86e5716385436bd930a16fd5acfd94b
Anton Tsvetkov <anton_tsvetkov@epam.com>
2021-02-09 10:06:11 +0300

    Add new tests for subdivision math expression

--- src/test/java/com/epam/laboratory/AppTest.java
+++ src/test/java/com/epam/laboratory/AppTest.java
@@ -2,17 +2,31 @@ package com.epam.laboratory;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 
 public class AppTest {
 
     private static final String initialString = "2 + 3 * 45.3 * 90 + 20 - 8/20 - sqrt4";
-    private static final String prioritizedString = "3 * 45.3 * 90 - 8/20 - sqrt4 + 2 + 20";
     private static final double answer = 12_250.6;
 
-    Calculator calculator = new Calculator();
+    private static final Calculator calculator = new Calculator();
 
+    @BeforeAll
     @Test
-    public void testCalculateMathExpression() {
+    public static void testExtractionNumbersFromMathExpression(){
+
+    }
+
+    @BeforeAll
+    @Test
+    public static void testExtractionArithmeticOperationsFromMathExpression(){
+
+    }
+
+    @AfterAll
+    @Test
+    public static void testCalculateMathExpression() {
         Assert.assertEquals(calculator.calculateMathExpression(initialString), answer, 0.0);
     }
 
