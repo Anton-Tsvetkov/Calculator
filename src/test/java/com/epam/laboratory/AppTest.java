66e53ad43a57da780770b64f3861f84e6fa20568
Anton Tsvetkov <anton_tsvetkov@epam.com>
2021-02-09 09:43:54 +0300

    Update test with main logic

--- src/test/java/com/epam/laboratory/AppTest.java
+++ src/test/java/com/epam/laboratory/AppTest.java
@@ -5,12 +5,15 @@ import org.junit.Test;
 
 public class AppTest {
 
-    private static final String initialString = "2 + 3 * 45.3 * 90 + 20 - 8 / 20 - sqrt4";
-    private static final String prioritizedString = "3*45.3*90-8/20-sqrt4+2+20";
+    private static final String initialString = "2 + 3 * 45.3 * 90 + 20 - 8/20 - sqrt4";
+    private static final String prioritizedString = "3 * 45.3 * 90 - 8/20 - sqrt4 + 2 + 20";
+    private static final double answer = 12_250.6;
+
+    Calculator calculator = new Calculator();
 
     @Test
-    public void testConversionAccordingToPriority() {
-        Assert.assertEquals(conversionAccordingToPriority(initialString), prioritizedString);
+    public void testCalculateMathExpression() {
+        Assert.assertEquals(calculator.calculateMathExpression(initialString), answer, 0.0);
     }
 
 }
