cd6b0d70974533c99589991af55ce28d1bc540dc
Anton Tsvetkov <anton_tsvetkov@epam.com>
2021-02-11 17:39:09 +0300

    Add own exceptions

---
+++ src/main/java/com/epam/laboratory/exceptions/DivisionByZeroException.java
@@ -0,0 +1,8 @@
+package com.epam.laboratory.exceptions;
+
+public class DivisionByZeroException extends ArithmeticException {
+
+    public DivisionByZeroException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/epam/laboratory/exceptions/IncorrectOperationEntryException.java
@@ -0,0 +1,8 @@
+package com.epam.laboratory.exceptions;
+
+public class IncorrectOperationEntryException extends Exception {
+
+    public IncorrectOperationEntryException(String message) {
+        super(message);
+    }
+}
