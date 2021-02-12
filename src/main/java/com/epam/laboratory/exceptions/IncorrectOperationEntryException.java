c90688f2cc429e6cc4880e55072c0c5535bad6cd
Anton Tsvetkov <anton_tsvetkov@epam.com>
2021-02-12 10:37:58 +0300

    Add valid operation check

--- src/main/java/com/epam/laboratory/exceptions/IncorrectOperationEntryException.java
+++ src/main/java/com/epam/laboratory/exceptions/IncorrectOperationEntryException.java
@@ -5,4 +5,10 @@ public class IncorrectOperationEntryException extends Exception {
     public IncorrectOperationEntryException(String message) {
         super(message);
     }
+
+    public static boolean notValidOperation(String str){
+        return str.length() > 1 && !str.contains("sqrt")
+                || (!str.equals("*") && !str.equals("/") && !str.equals("+") && !str.equals("-"));
+    }
+
 }
